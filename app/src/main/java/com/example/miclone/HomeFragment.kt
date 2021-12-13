0b0ad0713b7674206913aa6197498f27fce50891
VysaghKT <vysagh.kt@gadgeon.com>
2021-12-13 17:41:40 +0530

    checking if bluetooth is enabled

--- app/src/main/java/com/example/miclone/HomeFragment.kt
+++ app/src/main/java/com/example/miclone/HomeFragment.kt
@@ -1,19 +1,63 @@
 package com.example.miclone
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.miclone.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    private val bluetoothAdapter:BluetoothAdapter by lazy {
+        val bluetoothManager = requireContext().getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+
+        if (!bluetoothAdapter.isEnabled){
+            promptBluetoothEnable()
+        }
+
+        return binding.root
     }
 
+    private fun promptBluetoothEnable() {
+        val intent  = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        startActivityForResult(intent, ENABLE_BLUETOOTH_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            ENABLE_BLUETOOTH_REQUEST_CODE -> {
+                if (resultCode == Activity.RESULT_OK){
+                    if (!bluetoothAdapter.isEnabled){
+                        binding.bluetoothConnected.visibility = View.VISIBLE
+                    }else{
+                        binding.bluetoothConnected.visibility = View.GONE
+                    }
+                }else if (resultCode == Activity.RESULT_CANCELED){
+                    binding.bluetoothConnected.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+    companion object{
+        private const val ENABLE_BLUETOOTH_REQUEST_CODE = 1
+    }
 }
