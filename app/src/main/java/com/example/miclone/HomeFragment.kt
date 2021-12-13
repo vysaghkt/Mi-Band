6fe5a1a94ad1cb1b5e4d10c5f4fb9f2cb4a13de6
VysaghKT <vysagh.kt@gadgeon.com>
2021-12-13 18:04:45 +0530

    Connecting a pre-defined device

--- app/src/main/java/com/example/miclone/HomeFragment.kt
+++ app/src/main/java/com/example/miclone/HomeFragment.kt
@@ -1,17 +1,18 @@
 package com.example.miclone
 
 import android.app.Activity
-import android.app.AlertDialog
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothManager
+import android.bluetooth.*
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.miclone.Constants.MI_BAND_MAX_ADDRESS
 import com.example.miclone.databinding.FragmentHomeBinding
+import java.lang.IllegalArgumentException
 
 class HomeFragment : Fragment() {
 
@@ -22,6 +23,8 @@ class HomeFragment : Fragment() {
         bluetoothManager.adapter
     }
 
+    private var bluetoothGatt: BluetoothGatt? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,9 +33,31 @@ class HomeFragment : Fragment() {
 
         if (!bluetoothAdapter.isEnabled){
             promptBluetoothEnable()
+        }else {
+            connectDevice()
         }
 
         return binding.root
+    }
+
+    private fun connectDevice() {
+        try {
+            val device = bluetoothAdapter.getRemoteDevice(MI_BAND_MAX_ADDRESS)
+            device.connectGatt(requireContext(), false, bluetoothGattCallBack)
+        }catch (e: IllegalArgumentException){
+            Log.d(TAG,"Invalid Mac Address")
+        }
+    }
+
+    private val bluetoothGattCallBack = object : BluetoothGattCallback(){
+
+        override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+            if (newState == BluetoothProfile.STATE_CONNECTED){
+                Log.d(TAG,"Device with ${gatt.device.address} is connected")
+            }else if (newState == BluetoothProfile.STATE_DISCONNECTED){
+                Log.d(TAG,"Device Disconnected")
+            }
+        }
     }
 
     private fun promptBluetoothEnable() {
@@ -57,7 +82,13 @@ class HomeFragment : Fragment() {
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        bluetoothGatt?.close()
+        bluetoothGatt = null
+    }
     companion object{
         private const val ENABLE_BLUETOOTH_REQUEST_CODE = 1
+        private const val TAG = "MI_BAND"
     }
 }
