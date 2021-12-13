74712ab82a0b6426cb836f505d806d3b29b20c98
VysaghKT <vysagh.kt@gadgeon.com>
2021-12-13 16:40:05 +0530

    Setup Fragments for home and settings

--- app/src/main/java/com/example/miclone/MainActivity.kt
+++ app/src/main/java/com/example/miclone/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.miclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var bottomNav: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.fragmentContainerView)
+        bottomNav = findViewById(R.id.bottomNavigationView)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.settingsFragment
+            )
+        )
+
+        bottomNav.setupWithNavController(navController)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
